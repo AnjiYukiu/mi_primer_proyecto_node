bb7808b28e27ae95d507a20bc5f6419743eeadee
AnjiYukiu <110330448+AnjiYukiu@users.noreply.github.com>
2022-08-09 11:03:55 +0200

    Create Ejercicio2JavaScriptBasico.js

---
+++ Ejercicio2JavaScriptBasico.js
@@ -0,0 +1,12 @@
+const nombre = "Gerardo";
+const edad = 34;
+const dev = false;
+const fechaNac = new Date(1988, 4, 25);
+const libroFav = {
+    titulo: "El médico",
+    autor: "Noah Gordon",
+    fecha: new Date("august 7 1986"),
+    url: "https://es.wikipedia.org/wiki/El_m%C3%A9dico_(novela)"
+}
+
+console.log(libroFav);
