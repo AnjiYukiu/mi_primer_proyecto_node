a3f31e4d2152ed508a65cc12cdc40a92df40f16d
AnjiYukiu <110330448+AnjiYukiu@users.noreply.github.com>
2022-08-11 12:37:37 +0200

    Update EjerciciosTema5JS.js

--- EjerciciosTema5JS.js
+++ EjerciciosTema5JS.js
@@ -5,5 +5,4 @@ var alturaFloatRedondeo = alturaFloat.toFixed(0); // Altura en m redondeada haci
 var pesoFloatRedondeo = Math.floor(pesoFloat); // Peso redondeado hacia abajo, he tenido que buscar el atributo de la función Math porque con lo explicado en los vídeos no sabía hacerlo
 var comparacion = (Number.MAX_VALUE + 1) === Number.MAX_VALUE // Variable que compara números máximos
 
-console.log(comparacion)
 
